7f7b83520c2b419c96735b5922ddb6fb47d02237
HJC510 <kinghuang@apjcorp.com>
2026-05-25 14:45:18 +0800

    feat: test ci & cd

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      code: 0,
+      message: 'success',
+      data: 'Hello World!'
+    };
   }
 }
